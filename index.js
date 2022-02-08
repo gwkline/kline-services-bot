b4416ef21b959c8c01a474f8c1b6837bb5b627ce
Gavin Kline <77706736+gwkline@users.noreply.github.com>
2022-02-08 12:29:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ async function getOrders() {
             let custom_field = orders["body"][i]["custom_fields"]["value"]
             let price = (orders["body"][i]["quantity"] * orders["body"][i]["price"]) * 0.971 - 0.3
 
-            if (WHITELIST.includes(product) {
+            if (WHITELIST.includes(product)) {
                 custom_field = orders["body"][i]["custom_fields"][0]["value"]
             }
 
